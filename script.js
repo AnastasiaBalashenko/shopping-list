47831554381557a2f06ea4a95b5a8a80c9da8207
AnastasiaBalashenko <anastasiabalashenko@gmail.com>
2024-06-02 12:18:42 +0300

    Домашнее задание 4

--- script.js
+++ script.js
@@ -7,3 +7,33 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+
+const sendInput = document.querySelector('#input');
+const groceriesContainer = document.querySelector('.groceries');
+
+
+sendInput.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+        const groceriesText = sendInput.value;
+
+        const newGrocery = document.createElement('li');
+        newGrocery.classList.add('groceries');
+        newGrocery.textContent = groceriesText;
+
+        if (groceriesText != '') {
+            groceriesContainer.append(newGrocery);
+        }
+
+        sendInput.value = '';
+
+    }
+
+    const element = document.querySelectorAll('li');
+    for (let item of element) {
+        item.addEventListener('click', function() {
+            item.classList.toggle('done');
+        });
+    }
+
+});
